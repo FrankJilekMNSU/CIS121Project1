287e804c0fca23bfc6e3a2bf318d44114d0c3a89
FrankJilekMNSU <frankjilekmnsu@gmail.com>
2023-10-26 16:30:55 -0500

    player turn

--- player_turn.py
+++ player_turn.py
@@ -1,24 +1,32 @@
 from board import *
 
 class Player:
-	def __init__(self, in_board):
+	def __init__(self, in_board, name):
 		self.ship_board = in_board
 		self.attack_board = Board(create_empty_board(), board_type_enemy)
+		self.name
 		
 
-def player_turn(player_return):
-	while True:
-		player_input = input(f"{players[current_player]}, enter where you want to move: ")
-		#gets the current players input
-	
-		if gameover:
-			#checks to see if someone won
-			print(f"{players[current_player]} wins!!!!")
-			#whoever's turn it just was wins
-			break
-	
-		current_player = (current_player + 1) % len(players)
-		#switches to next player's turn assuming that nobody won
-		
-	
-	
+def player_turn(player1, player2):
+	valid_move = False
+	hit = 0
+	while not valid_move:
+		player_input = input(f"{player1.name} Attack: ")
+		if len(player_input) < 2 or len(player_input) > 3:
+			valid_move = False
+			print("Invalid input")
+		column = ord(player_input[0]) - 0x41
+		row = int(player_input[1:len(player_input)])-1
+		if column < 0 or column > board_height or row < 0 or row > board_width:
+			valid_move = False
+			print("Invalid location")
+		hit = player2.ship_board.try_hit(row, column)
+		if hit == board_move_invalid:
+			valid_move = False
+			print("Spot has already been attacked")
+		else:
+			valid_move = True
+	if hit == board_move_hit:
+		print("Hit!")
+	else:
+		print("Miss!")
