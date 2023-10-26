a7748aaaf2963f5eff3f9d3d85e43990a1bce66e
August Peterson <lw7157nc@go.minnstate.edu>
2023-10-26 16:09:20 -0500

    Player Turn

--- player_turn.py
+++ player_turn.py
@@ -1,20 +1,25 @@
-players = ["Player 1", "Player 2"]
+from board import *
 
-current_player = 0
-#Initialization of players
+class Player:
+	boards = [0,0]
+	def __init__(self, board1, board2):
+		board[0] = board1
+		board[1] = board2 
+		
 
-while True:
-	player_input = input(f"{players[current_player]}, enter where you want to move: ")
-	#gets the current players input
+def player_turn(player_return):
+	while True:
+		player_input = input(f"{players[current_player]}, enter where you want to move: ")
+		#gets the current players input
 	
-	if gameover:
-		#checks to see if someone won
-		print(f"{players[current_player]} wins!!!!")
-		#whoever's turn it just was wins
-		break
+		if gameover:
+			#checks to see if someone won
+			print(f"{players[current_player]} wins!!!!")
+			#whoever's turn it just was wins
+			break
 	
-	current_player = (current_player + 1) % len(players)
-	#switches to next player's turn assuming that nobody won
+		current_player = (current_player + 1) % len(players)
+		#switches to next player's turn assuming that nobody won
 		
 	
 	
